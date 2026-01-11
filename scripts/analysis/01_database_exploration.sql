4989643d9d01ae4c3acd3f488f893b138f5c4804
Mohamed Badwy Elsaid <115591574+MohamedBadwy360@users.noreply.github.com>
2026-01-11 18:55:30 +0300

    Clean up comments in database exploration SQL script
    Removed unnecessary comments about the purpose and tables used.

--- scripts/analysis/01_database_exploration.sql
+++ scripts/analysis/01_database_exploration.sql
@@ -5,10 +5,6 @@ Database Exploration
 Purpose:
     - To explore the structure of the database, including the list of tables and their schemas.
     - To inspect the columns and metadata for specific tables.
-
-Table Used:
-    - INFORMATION_SCHEMA.TABLES
-    - INFORMATION_SCHEMA.COLUMNS
 ===============================================================================
 */
 
